0170dcb82cb0c49f07992ded3d1d15fd0d6302e7
Touareb Mohamed <medtoub80@gmail.com>
2022-01-26 00:59:32 +0100

    Category now can delete/edit with notification messages (alerts)

--- Sellopedia/Controllers/AdminController.cs
+++ Sellopedia/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Sellopedia.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,12 +26,6 @@ namespace Sellopedia.Controllers
             return View(db.Categories.ToList());
         }
 
-        //[HttpGet]
-        //public ActionResult CreateCategory()
-        //{
-        //    return View();
-        //}
-
         [HttpPost]
         public ActionResult CreateCategory(string categoryName, string iconName)
         {
@@ -44,6 +39,45 @@ namespace Sellopedia.Controllers
             db.SaveChanges();
 
             //return Json(model, JsonRequestBehavior.AllowGet);
+            return RedirectToAction("Categories");
+        }
+
+
+        public ActionResult EditCategory(int? id, string categoryName, string iconName)
+        {
+            Category model = db.Categories.Find(id);
+
+            if (id == null || model == null)
+            {
+                TempData["edit_error"] = "The category you are trying to edit does not exist";
+                return RedirectToAction("Categories");
+            }
+
+            model.Name = categoryName;
+            model.Icon = iconName;
+
+            db.Categories.AddOrUpdate(model);
+            db.SaveChanges();
+
+            TempData["edit_success"] = $"Category has been edited successfully.";
+            return RedirectToAction("Categories");
+        }
+
+
+        public ActionResult DeleteCategory(int? id)
+        {
+            Category model = db.Categories.Find(id);
+
+            if(id == null || model == null)
+            {
+                TempData["delete_error"] = "Unable to delet category. Selected item does not exist.";
+                return RedirectToAction("Categories");
+            }
+
+            db.Categories.Remove(model);
+            db.SaveChanges();
+
+            TempData["delete_success"] = $"Deleted category <strong>\"{model.Name}\"</strong> successfully.";
             return RedirectToAction("Categories");
         }
     }
